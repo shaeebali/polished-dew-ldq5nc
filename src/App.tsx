f5c659fcb131dd956ce08632f7dd4118cf477223
shaeeb <shaeebali@gmail.com>
2024-12-28 12:25:01 +0000

    add navbar add styling

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,13 @@ import { Users } from "./components/Users";
 import "./input.css";
 import "./App.css";
 import { Hero } from "./components/Hero";
+import { NavBar } from "./components/NavBar";
 const queryClient = new QueryClient();
 
 export default function App() {
   return (
     <QueryClientProvider client={queryClient}>
+      <NavBar />
       <Hero />
       <Users />
     </QueryClientProvider>
