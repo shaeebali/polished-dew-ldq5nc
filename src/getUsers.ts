8cfefb3a6a5e9c62b363c02bddcae31a6e7743d8
shaeeb <shaeebali@gmail.com>
2024-12-28 11:15:20 +0000

    refactor async function

--- src/getUsers.ts
+++ src/getUsers.ts
@@ -5,8 +5,17 @@ export interface User {
   status: string;
 }
 
-export async function getUsers(): Promise<User[]> {
-  return (
-    await fetch("https://tech-interview-api-ultramed.vercel.app/users")
-  ).json();
-}
+// export async function getUsers(): Promise<User[]> {
+//   return (
+//     await fetch("https://tech-interview-api-ultramed.vercel.app/users")
+//   ).json();
+
+// }
+
+export const getUsers = async (): Promise<User[]> => {
+  const response = await fetch("https://tech-interview-api-ultramed.vercel.app/users");
+  const data = await response.json();
+  return data;
+};
+
+console.log(getUsers());
