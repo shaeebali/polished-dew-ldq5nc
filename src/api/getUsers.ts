df229c78f3fa158e57e07189c2d9d0446b318d11
shaeeb <shaeebali@gmail.com>
2025-01-14 11:43:09 +0000

    add try catch block to api async function

--- src/api/getUsers.ts
+++ src/api/getUsers.ts
@@ -7,12 +7,26 @@ import  { User } from "../types/User";
 
 // }
 
+
+
+// export const getUsers = async (): Promise<User[]> => {
+//   const response = await fetch("https://tech-interview-api-ultramed.vercel.app/users");
+//   const data = await response.json();
+//   return data;
+// };
+
 //Interface move to /types directory as being used as an import.
-
+//forgot to add a try catch block. Added it below:
 export const getUsers = async (): Promise<User[]> => {
-  const response = await fetch("https://tech-interview-api-ultramed.vercel.app/users");
-  const data = await response.json();
-  return data;
+  try {
+    const response = await fetch("https://tech-interview-api-ultramed.vercel.app/users");
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Error fetching users:", error);
+    throw error; // Rethrow the error to be handled by the caller if needed
+  }
 };
-
-
